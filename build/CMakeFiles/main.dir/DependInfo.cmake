1f5ccb449e99e353e651ad3962c32a12c83dc57e
wjh720 <wjh720.eric@gmail.com>
2018-01-05 15:52:14 +0800

    123

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/bmp.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/bmp.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/camera.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/camera.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/collision.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/collision.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/config.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/config.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/engine.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/engine.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/hitpointmap.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/hitpointmap.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/light.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/light.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/main.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/main.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/material.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/material.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/object.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/object.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/photontracer.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/photontracer.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/plane.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/plane.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/pointlight.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/pointlight.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/ppm.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/ppm.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/raytracer.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/raytracer.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/rectlight.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/rectlight.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/scene.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/scene.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/sphere.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/sphere.cpp.o"
+  "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/vector3.cpp" "/Users/wjh720/Desktop/now/SRT/Wavenet/Code/3Drender_wjh/build/CMakeFiles/main.dir/vector3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/bmp.cpp.o"
+  "CMakeFiles/main.dir/camera.cpp.o"
+  "CMakeFiles/main.dir/collision.cpp.o"
+  "CMakeFiles/main.dir/config.cpp.o"
+  "CMakeFiles/main.dir/engine.cpp.o"
+  "CMakeFiles/main.dir/hitpointmap.cpp.o"
+  "CMakeFiles/main.dir/light.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/material.cpp.o"
+  "CMakeFiles/main.dir/object.cpp.o"
+  "CMakeFiles/main.dir/photontracer.cpp.o"
+  "CMakeFiles/main.dir/plane.cpp.o"
+  "CMakeFiles/main.dir/pointlight.cpp.o"
+  "CMakeFiles/main.dir/ppm.cpp.o"
+  "CMakeFiles/main.dir/raytracer.cpp.o"
+  "CMakeFiles/main.dir/rectlight.cpp.o"
+  "CMakeFiles/main.dir/scene.cpp.o"
+  "CMakeFiles/main.dir/sphere.cpp.o"
+  "CMakeFiles/main.dir/vector3.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
